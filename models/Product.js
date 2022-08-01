bcf81e090b829868faacaa59edd035445b12b705
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-01 16:24:09 -0500

    added product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define columns - copy category.js set up. 
+    // see product-seeds for items
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+     },
+    price:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // defaultValue?
+     },
+     stock: {
+      type: DataTypes.BOOLEAN,
+     defaultValue: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // allowNull: false, need this??
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
