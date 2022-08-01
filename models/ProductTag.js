614807acb172346a29ac015e1e4c34f95ed128ce
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-01 16:35:57 -0500

    productTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns - copy id part. 
+    // look at product-tag seed for items- product_id & tag_id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // references for _id's
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+            // allowNull: false, need this??
+        references: {
+          model: 'tag',
+          key: 'id',
+        },
+    },
   },
   {
     sequelize,
