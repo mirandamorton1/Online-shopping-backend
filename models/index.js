49595e247cff52071c8f4630827262bb7905e973
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-01 16:50:44 -0500

    index model

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
+// Activity 26
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Categories.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsTo(Tags, {
+  through: ProductTag,
+  foreignKey: 'category_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  foreignKey: 'category_id',
+});
 
 module.exports = {
   Product,
